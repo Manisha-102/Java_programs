e03ab42ceb634be9eebb9ef8627e01e2e1f00d81
Manisha-102 <107210345+Manisha-102@users.noreply.github.com>
2022-06-11 19:59:25 +0530

    Add files via upload

---
+++ Area_Circle.java
@@ -0,0 +1,19 @@
+import java.util.Scanner;
+
+public class Area_Circle {
+    public static void main(String[] args) {
+        Scanner sc=new Scanner(System.in);
+
+        System.out.println("Enter radius of the circle:");
+        float r=sc.nextFloat();
+
+        //calculate area of circle
+        double Area=(3.14*r*r);//This is the formula for area of circle
+        System.out.println("Area of the circle is:-)"+Area);
+
+        //calculate circumstances of circle
+        double circum=(2*3.14*r);//This is the formula for area of circumstances
+        System.out.println("circumstances of circle is:-)"+circum);
+
+    }
+}
